e8df7dbeb2d3dbb0cd04eec026b93f88c34c8c5f
nurufu <>
2025-03-02 04:58:07 -0500

    added all cries to gen 5 Pokemon

--- common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen5/FrillishModel.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/client/render/models/blockbench/pokemon/gen5/FrillishModel.kt
@@ -9,6 +9,7 @@
 package com.cobblemon.mod.common.client.render.models.blockbench.pokemon.gen5
 
 import com.cobblemon.mod.common.client.render.models.blockbench.frame.HeadedFrame
+import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.CryProvider
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPose
 import com.cobblemon.mod.common.client.render.models.blockbench.pokemon.PokemonPoseableModel
 import com.cobblemon.mod.common.entity.PoseType
@@ -27,6 +28,8 @@ class FrillishModel (root: ModelPart) : PokemonPoseableModel(), HeadedFrame {
 
     lateinit var standing: PokemonPose
     lateinit var walk: PokemonPose
+
+    override val cryAnimation = CryProvider { _, _ -> bedrockStateful("frillish", "cry") }
 
     override fun registerPoses() {
         val blink = quirk { bedrockStateful("frillish", "blink") }
