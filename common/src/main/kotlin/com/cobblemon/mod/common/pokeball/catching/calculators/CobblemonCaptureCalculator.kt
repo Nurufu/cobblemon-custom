5caac7138f20ea81eacd0079ac186143b027f562
nurufu <>
2025-03-17 18:05:28 -0400

    added config toggle for inbattlemodifier

--- common/src/main/kotlin/com/cobblemon/mod/common/pokeball/catching/calculators/CobblemonCaptureCalculator.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/pokeball/catching/calculators/CobblemonCaptureCalculator.kt
@@ -58,7 +58,10 @@ object CobblemonCaptureCalculator: CaptureCalculator, CriticalCaptureProvider, P
         }
         // We don't have dark grass so we're just gonna pretend everything is that. Scratch that, without the pokedex it has issues.
         val darkGrass = 1F //if (thrower is ServerPlayerEntity) this.caughtMultiplierFor(thrower).roundToInt() else 1
-        val inBattleModifier = if (target.battleId != null) 1F else 0.5F
+        var inBattleModifier = if (target.battleId != null) 1F else 0.5F
+        if(!Cobblemon.config.inBattleModifier){
+            inBattleModifier = 1F
+        }
         val catchRate = getCatchRate(thrower, pokeBallEntity, target, pokemon.form.catchRate.toFloat())
         val validModifier = pokeBall.catchRateModifier.isValid(thrower, pokemon)
         val bonusStatus = when (pokemon.status?.status) {
@@ -75,6 +78,7 @@ object CobblemonCaptureCalculator: CaptureCalculator, CriticalCaptureProvider, P
             .behavior(thrower, pokemon)
             .mutator((3F * pokemon.hp - 2F * pokemon.currentHealth) * darkGrass * catchRate * inBattleModifier, ballBonus) / (3F * pokemon.hp)
         modifiedCatchRate *= bonusStatus * bonusLevel
+        Cobblemon.LOGGER.info(inBattleModifier)
         if (thrower is ServerPlayerEntity) {
             val highestLevelThrower = this.findHighestThrowerLevel(thrower, pokemon)
             if (highestLevelThrower != null && highestLevelThrower < pokemon.level) {
