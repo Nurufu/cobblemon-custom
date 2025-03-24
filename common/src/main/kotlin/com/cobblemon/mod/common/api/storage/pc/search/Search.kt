02a3d2fc67451308ca1b394406aeea748526713a
nurufu <>
2025-03-23 20:39:33 -0400

    added some almost missed search functions

--- common/src/main/kotlin/com/cobblemon/mod/common/api/storage/pc/search/Search.kt
+++ common/src/main/kotlin/com/cobblemon/mod/common/api/storage/pc/search/Search.kt
@@ -10,6 +10,7 @@ package com.cobblemon.mod.common.api.storage.pc.search
 
 import com.cobblemon.mod.common.Cobblemon
 import com.cobblemon.mod.common.api.pokemon.PokemonProperties
+import com.cobblemon.mod.common.api.pokemon.stats.Stat
 import com.cobblemon.mod.common.api.pokemon.stats.Stats
 import com.cobblemon.mod.common.pokemon.Gender
 import com.cobblemon.mod.common.pokemon.Pokemon
@@ -81,75 +82,103 @@ class Search(
                             4
                         ).toIntOrNull() ?: 0)
                     }
+                    //IV <= X
+                    "hp<=${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.HP)
+                    }
+                    "atk<=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.ATTACK)
+                    }
+                    "def<=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.DEFENCE)
+                    }
+                    "spa<=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPECIAL_ATTACK)
+                    }
+                    "spd<=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPECIAL_DEFENCE)
+                    }
+                    "spe<=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPEED)
+                    }
                     //IV >= X
                     "hp>=${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.HP]!! >= (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.HP)
                     }
                     "atk>=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.ATTACK]!! >= (filter.substring(
-                            5
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.ATTACK)
                     }
                     "def>=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.DEFENCE]!! >= (filter.substring(
-                            5
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.DEFENCE)
                     }
                     "spa>=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPECIAL_ATTACK]!! >= (filter.substring(
-                            5
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPECIAL_ATTACK)
                     }
                     "spd>=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPECIAL_DEFENCE]!! >= (filter.substring(
-                            5
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPECIAL_DEFENCE)
                     }
                     "spe>=${try{filter.substring(5)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPEED]!! >= (filter.substring(
-                            5
-                        ).toIntOrNull() ?: 0)
+                        ivGreaterOrEqual(pokemon, filter.substring(5).toIntOrNull() ?: 0, Stats.SPEED)
                     }
                     //IV > X
                     "hp>${try{filter.substring(3)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.HP]!! > (filter.substring(
-                            3
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(3).toIntOrNull() ?: 0, Stats.HP)
                     }
                     "atk>${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.ATTACK]!! > (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.ATTACK)
                     }
                     "def>${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.DEFENCE]!! > (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.DEFENCE)
                     }
                     "spa>${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPECIAL_ATTACK]!! > (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPECIAL_ATTACK)
                     }
                     "spd>${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPECIAL_DEFENCE]!! > (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPECIAL_DEFENCE)
                     }
                     "spe>${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
-                        pokemon.ivs[Stats.SPEED]!! > (filter.substring(
-                            4
-                        ).toIntOrNull() ?: 0)
+                        ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPEED)
+                    }
+                    //IV < X
+                    "hp<${try{filter.substring(3)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(3).toIntOrNull() ?: 0, Stats.HP)
+                    }
+                    "atk<${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.ATTACK)
+                    }
+                    "def<${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.DEFENCE)
+                    }
+                    "spa<${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPECIAL_ATTACK)
+                    }
+                    "spd<${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPECIAL_DEFENCE)
+                    }
+                    "spe<${try{filter.substring(4)}catch(e:Exception){null}}" -> PokemonFilter { pokemon ->
+                        !ivGreater(pokemon, filter.substring(4).toIntOrNull() ?: 0, Stats.SPEED)
                     }
                     //X amount of perfect IVs
-                    "perfect", "6x" -> PokemonFilter { pokemon -> ivCheck(pokemon,6) }
-                    "5x" -> PokemonFilter { pokemon -> ivCheck(pokemon, 5) }
-                    "4x" -> PokemonFilter { pokemon -> ivCheck(pokemon, 4) }
-                    "3x" -> PokemonFilter { pokemon -> ivCheck(pokemon, 3) }
-                    "2x" -> PokemonFilter { pokemon -> ivCheck(pokemon, 2) }
-                    "1x" -> PokemonFilter { pokemon -> ivCheck(pokemon, 1) }
+                    "perfect", "6x" -> PokemonFilter { pokemon -> ivPerfect(pokemon,6) }
+                    "5x" -> PokemonFilter { pokemon -> ivPerfect(pokemon, 5) }
+                    "4x" -> PokemonFilter { pokemon -> ivPerfect(pokemon, 4) }
+                    "3x" -> PokemonFilter { pokemon -> ivPerfect(pokemon, 3) }
+                    "2x" -> PokemonFilter { pokemon -> ivPerfect(pokemon, 2) }
+                    "1x" -> PokemonFilter { pokemon -> ivPerfect(pokemon, 1) }
+                    //IV % Total
+                    "%>${try{filter.substring(2)}catch(e:Exception){null}}" -> PokemonFilter{ pokemon ->
+                        ivSum(pokemon, filter.substring(2).toIntOrNull() ?: 0)
+                    }
+                    "%<${try{filter.substring(2)}catch(e:Exception){null}}" -> PokemonFilter{ pokemon ->
+                        !ivSum(pokemon, filter.substring(2).toIntOrNull() ?: 0)
+                    }
+                    //Quick 5x31 1x0
+                    "0hp" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.HP)}
+                    "0atk" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.ATTACK)}
+                    "0def" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.DEFENCE)}
+                    "0spa" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.SPECIAL_ATTACK)}
+                    "0spd" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.SPECIAL_DEFENCE)}
+                    "0spe" -> PokemonFilter { pokemon -> iv5X(pokemon, Stats.SPEED)}
                     else -> PokemonFilter { pokemon -> PokemonProperties.parse(filter).matches(pokemon) }
                 }
                 if (inverted) {
@@ -161,7 +190,23 @@ class Search(
 
             return Search(filters)
         }
-        private fun ivCheck(pokemon: Pokemon, amt: Int): Boolean{
+        private fun ivGreater(pokemon: Pokemon, x: Int, s: Stat): Boolean{
+            return pokemon.ivs[s]!! > x
+        }
+
+        private fun ivGreaterOrEqual(pokemon: Pokemon, x: Int, s: Stat): Boolean{
+            return pokemon.ivs[s]!! >= x
+        }
+
+        private fun iv5X(pokemon: Pokemon, s: Stat): Boolean{
+            var total = 0
+            for(i in pokemon.ivs){
+                total += i.value
+            }
+            return pokemon.ivs[s]!! == 0 && total == 155
+        }
+
+        private fun ivPerfect(pokemon: Pokemon, amt: Int): Boolean{
             var count = 0
             for(s in pokemon.ivs){
                 if(s.value == 31){
@@ -169,6 +214,17 @@ class Search(
                 }
             }
             return count>=5
+        }
+
+        private fun ivSum(pokemon: Pokemon, p: Int): Boolean{
+            var total= 0f
+
+            for(s in pokemon.ivs){
+                total += s.value
+            }
+            total = (total/186f)*100
+            Cobblemon.LOGGER.info(total.toString())
+            return p<=total
         }
     }
 
